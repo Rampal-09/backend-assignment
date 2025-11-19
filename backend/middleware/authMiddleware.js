a728b0dcd1984a3084fc534353c4fc656ed9ea2a
Rampal-09 <rampaltanwar0929@gmail.com>
2025-11-19 15:49:36 +0530

    feat: added auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.header.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        type: "unauthorized",
+        message: "no token provided",
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
