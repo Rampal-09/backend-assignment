a87b1c1f9c4c19e39bd238d43ffe8d2f257020a1
Rampal-09 <rampaltanwar0929@gmail.com>
2025-11-19 14:47:30 +0530

    fix: updated backend app.js and package.json

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,29 @@
- 
+require("dotenv").config();
+const express = require("express");
+
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(
+  cors({
+    origin: "https://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongoDb connected");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed!");
+    console.error("Reason:", error.message);
+  });
